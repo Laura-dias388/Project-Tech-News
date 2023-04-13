411f9e745fdef17aab9e0e02bb602e9062478eaa
laura <ldiasvitaltoledo8@gmail.com>
2023-04-13 18:14:23 -0300

    Requisito 5, concluído

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -3,6 +3,7 @@ import requests
 import time
 from bs4 import BeautifulSoup
 import re
+from tech_news.database import create_news
 
 
 # Requisito 1
@@ -61,5 +62,19 @@ def scrape_news(html_content):
 
 
 # Requisito 5
-def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+def get_tech_news(n):
+    response_fetch = fetch("https://blog.betrybe.com/")
+    get_all_links = scrape_updates(response_fetch)
+    get_next_link = scrape_next_page_link(response_fetch)
+    while len(get_all_links) < n:
+        get_next_response_fetch = fetch(get_next_link)
+        new_response_fetch_links = scrape_updates(get_next_response_fetch)
+        get_all_links.extend(new_response_fetch_links)
+        get_next_link = scrape_next_page_link(get_next_response_fetch)
+    new_response = [scrape_news(fetch(link)) for link in get_all_links[:n]]
+    return create_new_response_db(new_response)
+
+
+def create_new_response_db(new_response):
+    create_news(new_response)
+    return new_response
