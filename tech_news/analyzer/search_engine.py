299140c83fabfa7656dd51815319589181b1d917
laura <ldiasvitaltoledo8@gmail.com>
2023-04-13 23:40:16 -0300

    Requisito 8, concluído

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 7
@@ -9,9 +10,17 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.strptime(date, '%Y-%m-%d')
+    except ValueError:
+        raise ValueError("Data inválida")
+    date_formatting = datetime.strptime(
+        date, '%Y-%m-%d').strftime('%d/%m/%Y')
+    news = search_news({'timestamp': {'$eq': date_formatting}})
+    return [(new['title'], new['url']) for new in news]
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    query = {"categories": {"$regex": category.capitalize()}}
+    return [(n["title"], n["url"]) for n in search_news(query)]
