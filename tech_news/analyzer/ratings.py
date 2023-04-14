a36a932349d2031e8c9097676196f535b241f296
laura <ldiasvitaltoledo8@gmail.com>
2023-04-14 00:36:27 -0300

    Requisito 10, concluído

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,15 @@
+from collections import Counter
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    categories = [news['category'] for news in find_news()]
+    if not categories:
+        return []
+    counts_news_category = Counter(categories)
+    top_categories = sorted(
+        counts_news_category.items(),
+        key=lambda x: (-x[1], x[0].lower())
+    )
+    return [category for category, _ in top_categories[:5]]
